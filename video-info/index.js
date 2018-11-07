c852167e420641b62ecaf046feba549747b68c82
Mike Upton <uptonm@wit.edu>
2018-11-07 13:03:17 -0500

    Electron now shows video link through ffmpeg

--- video-info/index.js
+++ video-info/index.js
@@ -1,17 +1,15 @@
 const electron = require("electron");
 const { app, BrowserWindow, ipcMain } = electron;
-const fluent_ffmpeg = require("fluent-ffmpeg");
+const ffmpeg = require("fluent-ffmpeg");
 
+let mainWindow;
 app.on("ready", () => {
-  const view = new BrowserWindow({});
-  view.loadURL(`file://${__dirname}/index.html`);
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on("video:submit", (event, path) => {
-  let ffmpeg = fluent_ffmpeg(path).setFfprobePath(
-    "C:\\ffmpeg-3.3.3\\bin\\ffprobe.exe"
-  );
   ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log(metadata);
+    mainWindow.webContents.send("video:metadata", metadata.format.duration);
   });
 });
