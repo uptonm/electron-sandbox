daeb4996d23554b31127562035a55693494a4f28
Mike Upton <uptonm@wit.edu>
2018-11-08 12:36:22 -0500

    Continuing work on todo app

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,22 +2,38 @@ const electron = require("electron");
 
 const { app, BrowserWindow, Menu } = electron;
 
-let mainWindow;
+let mainWindow, addWindow;
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on("close", () => app.quit());
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    height: 200,
+    width: 300,
+    title: "Add New Todo"
+  });
+  addWindow.loadURL(`file://${__dirname}/form.html`);
+}
+
 const menuTemplate = [
   {
     label: "File",
     submenu: [
-      { label: "New Todo" },
+      {
+        label: "New Todo",
+        click() {
+          createAddWindow();
+        }
+      },
       {
         label: "Quit",
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
         click() {
           app.quit();
         }
@@ -28,4 +44,20 @@ const menuTemplate = [
 
 if (process.platform === "darwin") {
   menuTemplate.unshift({});
+}
+
+if (process.env.NODE_ENV !== "production") {
+  menuTemplate.push({
+    label: "View",
+    submenu: [
+      {
+        label: "Toggle Developer Tools",
+        accelerator:
+          process.platform === "darwin" ? "Command+Alt+I" : "Ctrl+Shift+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
 }
