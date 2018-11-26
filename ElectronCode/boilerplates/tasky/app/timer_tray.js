3b08b6fc4a069f3665926e7ce7fbb84cb2d27b5b
Mike Upton <uptonm@wit.edu>
2018-11-26 13:04:45 -0500

    Added context menu for quitting tray application

--- ElectronCode/boilerplates/tasky/app/timer_tray.js
+++ ElectronCode/boilerplates/tasky/app/timer_tray.js
@@ -1,5 +1,5 @@
 const electron = require("electron");
-const { Tray } = electron;
+const { Tray, Menu, app } = electron;
 
 class TimerTray extends Tray {
   constructor(iconPath, mainWindow) {
@@ -8,7 +8,8 @@ class TimerTray extends Tray {
     this.mainWindow = mainWindow;
 
     this.setToolTip("Timer App");
-    this.on("click", this.onClick);
+    this.on("click", this.onClick.bind(this));
+    this.on("right-click", this.onRightClick.bind(this));
   }
 
   onClick(event, bounds) {
@@ -26,6 +27,18 @@ class TimerTray extends Tray {
       });
       this.mainWindow.show();
     }
+  }
+
+  onRightClick() {
+    const menuConfig = Menu.buildFromTemplate([
+      {
+        label: "Quit",
+        click: () => {
+          app.quit();
+        }
+      }
+    ]);
+    this.popUpContextMenu(menuConfig);
   }
 }
 
