f720ad57d3f35605e361ecd234a3236f3bb668e1
Mike Upton <uptonm@wit.edu>
2018-11-26 12:49:15 -0500

    Refactored Timer to new Class

---
+++ ElectronCode/boilerplates/tasky/app/timer_tray.js
@@ -0,0 +1,30 @@
+const electron = require("electron");
+const { Tray } = electron;
+
+class TimerTray extends Tray {
+  constructor(iconPath, mainWindow) {
+    super(iconPath);
+
+    this.mainWindow = mainWindow;
+    this.on("click", this.onClick);
+  }
+
+  onClick(event, bounds) {
+    const { x, y } = bounds;
+    const { height, width } = this.mainWindow.getBounds();
+    const yPos = process.platform === "darwin" ? y : y - height;
+    if (this.mainWindow.isVisible()) {
+      this.mainWindow.hide();
+    } else {
+      this.mainWindow.setBounds({
+        x: x - width / 2,
+        y: yPos,
+        height: height,
+        width: width
+      });
+      this.mainWindow.show();
+    }
+  }
+}
+
+module.exports = TimerTray;
--- ElectronCode/boilerplates/tasky/index.js
+++ ElectronCode/boilerplates/tasky/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const electron = require("electron");
 const { app, BrowserWindow, Tray } = electron;
+const TimerTray = require("./app/timer_tray");
 
 let mainWindow, tray;
 
@@ -17,21 +18,5 @@ app.on("ready", () => {
   const iconName =
     process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  tray = new Tray(iconPath);
-  tray.on("click", (event, bounds) => {
-    const { x, y } = bounds;
-    const { height, width } = mainWindow.getBounds();
-    const yPos = process.platform === "darwin" ? y : y - height;
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      mainWindow.setBounds({
-        x: x - width / 2,
-        y: yPos,
-        height: height,
-        width: width
-      });
-      mainWindow.show();
-    }
-  });
+  tray = new TimerTray(iconPath, mainWindow);
 });
