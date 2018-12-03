d661932386cabe3da6d4d3cc468e8e61765b0088
Mike Upton <uptonm@wit.edu>
2018-12-03 12:48:08 -0500

    Refactor for importing multiple videos - Implemented ffmpeg and lodash for the import

--- ElectronCode/boilerplates/convert/index.js
+++ ElectronCode/boilerplates/convert/index.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const electron = require("electron");
 const ffmpeg = require("fluent-ffmpeg");
 
@@ -17,5 +18,22 @@ app.on("ready", () => {
 });
 
 ipcMain.on("videos:added", (event, videos) => {
-  console.log(videos);
+  /* Single Video Format */
+  // const promise = new Promise((resolve, reject) => {
+  //   ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
+  //     resolve(metadata);
+  //   });
+  // });
+  // promise.then(metadata => console.log(metadata));
+
+  /* Refactor for Multiple Videos */
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        resolve(metadata);
+      });
+    });
+  });
+
+  Promise.all(promises).then(results => console.log(results));
 });
