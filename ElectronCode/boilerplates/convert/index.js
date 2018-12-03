5ecddea78f1d520fd7ba3a7ec5aec9da467d84ea
Mike Upton <uptonm@wit.edu>
2018-12-03 12:24:10 -0500

    Initial Commit for Electron Video Converter Project

---
+++ ElectronCode/boilerplates/convert/index.js
@@ -0,0 +1,21 @@
+const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+      backgroundThrottling: false
+    }
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on("videos:added", (event, videos) => {
+  console.log(videos);
+});
